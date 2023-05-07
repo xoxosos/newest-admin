07fda313c884ab406fa207e343edfbf5c3b6f13f
CodeForBetter <xoxosos666@gmail.com>
2023-05-07 12:20:00 +0800

    feat: 新增导入

--- components.d.ts
+++ components.d.ts
@@ -15,6 +15,7 @@ declare module '@vue/runtime-core' {
     ACheckbox: typeof import('ant-design-vue/es')['Checkbox']
     ACol: typeof import('ant-design-vue/es')['Col']
     AConfigProvider: typeof import('ant-design-vue/es')['ConfigProvider']
+    ADatePicker: typeof import('ant-design-vue/es')['DatePicker']
     ADivider: typeof import('ant-design-vue/es')['Divider']
     ADrawer: typeof import('ant-design-vue/es')['Drawer']
     ADropdown: typeof import('ant-design-vue/es')['Dropdown']
